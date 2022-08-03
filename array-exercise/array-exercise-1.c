03c84c3db658fce9063ff3517477dae6d6b38b33
Ruezma <marvelinettou@gmail.com>
2022-08-03 18:47:51 +0700

    Mengimport file array-exercise-1 dari onlineGDB

---
+++ array-exercise/array-exercise-1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int data[10] = {45, 20, 30, 100, 95, 70, 75, 55, 85, 90};
+    int i, sum = 0; 
+    
+    //Calculate total data
+    for (i = 0; i < sizeof(data) / sizeof(*data); i++){
+        sum += data[i];
+    }
+    printf("Summation : %d\n", sum);
+    printf("Average : %.2u\n", sum / (sizeof(data) / sizeof(*data)));
+    
+    //Find the maximum value
+    sum = 0;
+    for (i = 0; i < sizeof(data) / sizeof(*data); i++){
+        if (sum < data[i]){
+            sum = data[i];
+        }
+    }
+    printf("Maximum : %d\n", sum);
+    
+    //Find the minimum value
+    for (i = 0; i < sizeof(data) / sizeof(*data); i++){
+        if (sum > data[i]){
+            sum = data[i];
+        }
+    }
+    printf("Minimum : %d\n", sum);
+    return 0;
+}
